5efb94c62865e0ce1d687c5f13372129aa7333a7
insub4067 <vb15@kakao.com>
2022-10-02 22:27:04 +0900

    [Feat] Feed View Mock Image Data
    2-6

--- lib/feed.dart
+++ lib/feed.dart
@@ -2,9 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Feed extends StatefulWidget {
-  const Feed({
-    Key? key,
-  }) : super(key: key);
+  const Feed({Key? key, required this.imageUrl}) : super(key: key);
+
+  final String imageUrl;
 
   @override
   State<Feed> createState() => _FeedState();
@@ -23,7 +23,7 @@ class _FeedState extends State<Feed> {
           borderRadius: BorderRadius.circular(8),
           // 이미지
           child: Image.network(
-            'https://cdn2.thecatapi.com/images/6bt.jpg',
+            widget.imageUrl,
             width: 100,
             height: 100,
             fit: BoxFit.cover,
